607fe2f3815aa6d669d189b729ed3ff0c367fa36
Alex Finnarn <alexander.finnarn@du.edu>
2025-10-30 16:35:37 -0400

    try dummy test to see it is targeted and passes

--- tests/playwright/e2e/banners.spec.ts
+++ tests/playwright/e2e/banners.spec.ts
@@ -13,14 +13,14 @@ test.describe('@banners - Login and out Tests', () => {
     //    Content > Add content > Alert
     // await page.getByRole('link', { name: 'Content' }).click();
     await page.locator('[data-drupal-link-system-path="admin/content"]').click();
-    await page.locator('[data-drupal-link-system-path="node/add"]').click();
-    await page.getByRole('link', { name: 'Alert' }).click();
+    // await page.locator('[data-drupal-link-system-path="node/add"]').click();
+    // await page.getByRole('link', { name: 'Alert' }).click();
 
     // Select Informational for Alert Type, which is called "notification" in the select list.
-    await page.selectOption(
-      'select[data-drupal-selector="edit-field-alert-type"]',
-      'notification'
-    );
+    // await page.selectOption(
+    //   'select[data-drupal-selector="edit-field-alert-type"]',
+    //   'notification'
+    // );
 
     // Enter “Hello” for the First Word
     //
