ab38cd7262cb4bf2354c6b024e33ee2b04b0f07e
Alex Finnarn <alexander.finnarn@du.edu>
2025-11-10 12:48:56 -0500

    try shared workflows

--- tests/playwright/e2e/banners.spec.ts
+++ tests/playwright/e2e/banners.spec.ts
@@ -5,14 +5,14 @@ test.describe('@banners - Login and out Tests', () => {
   const administrator = getRole('administrator');
 
   // @source https://ducloudwiki.atlassian.net/wiki/spaces/DS/pages/1115062392/UAT+-+Alerts+local
-  test('AL1 - Create a new Alert node', async ({page, context}) => {
+  test.skip('AL1 - Create a new Alert node', async ({page, context}) => {
     // 1. Authenticate as qa_site_admin.
     await logInViaForm(page, context, administrator);
 
     // 2. Navigate to the "add Alert" form using the Admin toolbar.
     //    Content > Add content > Alert
-    // await page.getByRole('link', { name: 'Content' }).click();
-    await page.locator('[data-drupal-link-system-path="admin/content"]').click();
+    await page.getByRole('link', { name: 'Content', exact: true }).click();
+    // await page.locator('[data-drupal-link-system-path="admin/content"]').click();
     // await page.locator('[data-drupal-link-system-path="node/add"]').click();
     // await page.getByRole('link', { name: 'Alert' }).click();
 
